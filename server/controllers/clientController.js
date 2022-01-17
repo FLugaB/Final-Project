5f55638c68c5f931f1ed7c6955b248b6d0ce6333
F Luga <fernandus.luga@gmail.com>
2022-01-17 20:12:45 +0700

    Add Register Client

---
+++ server/controllers/clientController.js
@@ -0,0 +1,65 @@
+const { User, Profile } = require(`../models/index`);
+const { sequelize } = require('../models')
+
+const clientRegister = async (req, res, next) => {
+    const transaction = await sequelize.transaction();
+
+    try {
+        const {  email, password, fullName, birthdate, gender, address, photoProfile, phoneNumber } = req.body
+        const addUser = await User.create({ email, password, role: "Client",  }, { transaction });
+
+        if (!addUser) throw { name: `USER_NOT_FOUND` }
+
+        const addProfile = await Profile.create({
+            fullName,
+            birthdate,
+            gender,
+            address,
+            photoProfile,
+            phoneNumber,
+            userId: addUser.id
+        }, { transaction })
+
+        await transaction.commit();
+        res.status(201).json({
+            id: addUser.id,
+            email: addUser.email,
+            fullName: addProfile.fullName
+        });
+    } catch (error) {
+        console.log(error)
+        next(error);
+        await transaction.rollback()
+    }
+};
+
+const clientLogin = async (req, res, next) => {
+    try {
+        const {  email, password } = req.body
+
+        const findUser = await User.findOne({
+            where: {
+                email: email
+            }
+        });
+
+        if (!findUser) throw { name: `USER_NOT_FOUND` }
+
+        const verfyPass = compareHash(password, findUser.password)
+
+        if (!verfyPass) throw { name: `USER_NOT_FOUND` }
+
+        const payload = { id: findUser.id, email: findUser.email }
+
+        const access_token = getToken(payload)
+
+        res.status(200).json({access_token});
+    } catch (error) {
+        next(error);
+    }
+};
+
+module.exports = {
+    clientRegister,
+    clientLogin
+}
---
+++ server/migrations/20220117124908-create-profile.js
@@ -0,0 +1,57 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Profiles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      fullName: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      birthdate: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      gender: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      address: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      photoProfile: {
+        type: Sequelize.STRING
+      },
+      phoneNumber: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id'
+        },
+        onUpdate: 'cascade',
+        onDelete: 'cascade' 
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Profiles');
+  }
+};
