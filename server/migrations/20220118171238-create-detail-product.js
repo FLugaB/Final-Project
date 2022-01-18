c2938f0bcee009691d7442ae48a3edc563642125
Melisa <zheriztin@gmail.com>
2022-01-19 01:40:03 +0700

    detail product

---
+++ server/migrations/20220118171238-create-detail-product.js
@@ -0,0 +1,51 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('DetailProducts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      ProductId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Products',
+          key: 'id'
+        },
+        onUpdate: 'cascade',
+        onDelete: 'cascade'
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      price: {
+        type: Sequelize.INTEGER
+      },
+      stock: {
+        type: Sequelize.INTEGER
+      },
+      category: {
+        type: Sequelize.STRING
+      },
+      imageUrl: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('DetailProducts');
+  }
+};
---
+++ server/models/detailproduct.js
@@ -0,0 +1,77 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class DetailProduct extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  DetailProduct.init({
+    ProductId: {
+      allowNull: false,
+      type:DataTypes.INTEGER,
+      validate: {
+        notEmpty: { msg: "Product is required" },
+        notNull: { msg: "Product is required" }
+      }
+    },
+    name: {
+      allowNull: false,
+      type:DataTypes.STRING,
+      validate: {
+        notEmpty: { msg: "Name is required" },
+        notNull: { msg: "Name is required" }
+      }
+    },
+    price: {
+      type:DataTypes.INTEGER,
+      validate: {
+        notEmpty: { msg: "price is required" },
+        min: { args: 1, msg: "Price can't be 0"},
+      },
+
+    },
+    stock:  {
+      type:DataTypes.INTEGER,
+      validate: {
+        notEmpty: { msg: "stock is required" },
+        min: { args: [0], msg: "Stock can't be lower then 0"},
+      }
+    },
+    category: {
+      allowNull: false,
+      type:DataTypes.STRING,
+      validate: {
+        notEmpty: { msg: "Category is required" },
+        notNull: { msg: "Category is required" }
+      }
+    },
+    imageUrl: {
+      allowNull: false,
+      type:DataTypes.STRING,
+      validate: {
+        notEmpty: { msg: "Image is required" },
+        notNull: { msg: "Image is required" }
+      }
+    },
+    description: {
+      allowNull: false,
+      type:DataTypes.STRING,
+      validate: {
+        notEmpty: { msg: "Description is required" },
+        notNull: { msg: "Description is required" }
+      }
+    },
+  }, {
+    sequelize,
+    modelName: 'DetailProduct',
+  });
+  return DetailProduct;
+};
