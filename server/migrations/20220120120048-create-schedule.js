26f6fae3168835ad74fd02cf612f58c07a380174
ingabd <hasanabdulrachman@gmail.com>
2022-01-20 19:04:14 +0700

    create schedule table, define its association to User

---
+++ server/migrations/20220120120048-create-schedule.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Schedules', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      day: {
+        type: Sequelize.STRING
+      },
+      hour: {
+        type: Sequelize.STRING
+      },
+      price: {
+        type: Sequelize.INTEGER
+      },
+      hospital: {
+        type: Sequelize.STRING
+      },
+      sip: {
+        type: Sequelize.STRING
+      },
+      UserId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Schedules');
+  }
+};
