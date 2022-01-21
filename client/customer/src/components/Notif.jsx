62e21bd382aab6c5e0d4674ac846c1bf47eb214b
F Luga <fernandus.luga@gmail.com>
2022-01-21 21:20:40 +0700

    Add Midtrans EndPoint if Finished Payment

---
+++ client/customer/src/components/Notif.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Notif = () => {
+
+    
+
+    return (
+        <div className="mt-5 pt-5">
+            <h1>Notif</h1>
+        </div>
+    );
+};
+
+export default Notif;
--- server/app.js
+++ server/app.js
@@ -1,13 +1,14 @@
 if (process.env.NODE_ENV !== "production") {
     require('dotenv').config();
 }
-
 const express = require("express");
 const app = express();
+const logger = require("morgan");
 const cors = require('cors')
 const route = require("./routes");
 
 app.use(cors())
+app.use(logger("dev"));
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
