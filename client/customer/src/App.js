f79ee762b84f6d992f1f0d5fa98f83fc2fe5bf0f
ludfiwahyu <ludfiwahyuh@gmail.com>
2022-01-21 14:52:14 +0700

    client-customers Added Router and  Video Chat

--- client/customer/src/App.js
+++ client/customer/src/App.js
@@ -1,10 +1,21 @@
 import "./App.css";
 import Dashboard from "./pages/Dashboard.jsx";
+import { Routes, Route } from "react-router-dom";
+// import { auth } from './firebase.js'
+// import { useAuthState } from 'react-firebase-hooks/auth'
+
+import VideoCall from "./pages/video/Meeting.jsx";
+import JoinMeeting from "./pages/video/Join.jsx";
 
 function App() {
   return (
     <div className="App">
-      <Dashboard />
+      <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/start-video" element={<JoinMeeting />} />
+          <Route path="/video/:id" element={<VideoCall />} />
+      </Routes>
+      {/* <Dashboard /> */}
     </div>
   );
 }
