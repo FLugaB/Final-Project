09f050fd706b4e55e9b75c8c5001c4ec0aeb1967
Melisa <zheriztin@gmail.com>
2022-01-25 05:22:15 +0700

    nambah controller

---
+++ server/controllers/chatProductControlleer.js
@@ -0,0 +1,59 @@
+const { User, Product, Voucher, Schedule } = require("../models")
+
+module.exports = class Controller {
+
+  static  voucherUsed = async (req, res, next) => {
+    try {
+        const findAllTicket = await Voucher.findAll({
+            where: {
+                [Op.and]: [
+                    { DoctorId: req.auth.id }, 
+                    { status: `used` }
+                ], 
+            },
+        })
+
+        if (findAllTicket.length < 1 ) {
+            const text = "There is No Ticket yet.."
+            res.status(200).json({text})
+        } else {
+            res.status(200).json(findAllTicket)
+        }
+    } catch (error) {
+        next(error)
+    }
+  }
+
+  static voucherCompleted =  async (req, res, next) => {
+    try {
+        const findAllTicket = await Voucher.findAll({
+            where: {
+                [Op.and]: [
+                    { DoctorId: req.auth.id }, 
+                    { status: `completed` }
+                ], 
+            },
+        })
+
+        if (findAllTicket.length < 1 ) {
+            const text = "There is No Ticket yet.."
+            res.status(200).json({text})
+        } else {
+            res.status(200).json(findAllTicket)
+        }
+    } catch (error) {
+        next(error)
+    }
+  }
+
+  static voucherUsedByToken = async (req, res, next) => {
+    
+    try {
+
+        // Nanti Setelah Client Ke tendang atau di kick oleh dokter atau waktu consul habis maka di patch status voucher tersebut menajadi Completed
+        
+    } catch (error) {
+        next(error)
+    }
+  }
+}
