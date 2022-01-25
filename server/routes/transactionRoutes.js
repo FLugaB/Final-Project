ca816b0e2cc73d9f8e6aa5a7795290e1e2e9810b
F Luga <fernandus.luga@gmail.com>
2022-01-25 13:10:11 +0700

    Add 10% Test

--- server/routes/transactionRoutes.js
+++ server/routes/transactionRoutes.js
@@ -1,7 +1,7 @@
 const route = require('express').Router()
 const { authentication, authorization, authorizationCMS } = require("../middlewere/auth")
 const { TransactionController } = require(`../controllers/transactionController`)
-const { requestSnapToken, updateStatusTransactions } = require('../apis/midtransController')
+const { requestSnapToken } = require('../apis/midtransController')
 
 
 
@@ -26,7 +26,7 @@ route.get('/account/status-transactions', [authentication, authorization], Trans
 
 // update status transaction, status berubah ke failed if 20m gk bayar 
 // OR update Status OrderProduct
-route.patch('/account/status-transactions/:orderId', [authentication, authorization], updateStatusTransactions)
+// route.patch('/account/status-transactions/:orderId', [authentication, authorization], updateStatusTransactions)
 
 // trigger by notification
 route.post('/notification/handling', TransactionController.notificationTransaction);
