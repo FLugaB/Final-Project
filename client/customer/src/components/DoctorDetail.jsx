17ce5e564e12d02d9c73fd6494234f2c4830bae9
Melisa <zheriztin@gmail.com>
2022-01-26 19:32:54 +0700

    doctor profile

--- client/customer/src/components/DoctorDetail.jsx
+++ client/customer/src/components/DoctorDetail.jsx
@@ -1,29 +1,148 @@
 import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom'
+import { useParams , Link} from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
+import './doctorDetail.css'
+import {fetchDoctorDetail} from "../store/actionCreator/doctors.js"
 
 
 const DoctorDetail = () => {
 
     const params = useParams()
 
-    console.log(params.doctors,` DETAIL`)
-
     const [localLoad, setLocalLoad] = useState(true)
     // const { } = useSelector( (state) => state.doctor)
     
-    // const dispatach = useDispatch();
-
-    // useEffect(() => dispatch(fetchProduct(params)), [params.id]);
+    const dispatch = useDispatch();
+    const data = useSelector(state => state.doctors.doctorDetail)
+    const {id} = useParams()
+    useEffect(() => {dispatch(fetchDoctorDetail(id))},[]);
     // useEffect(() => pageLoaded(), [beforeUpdateProduct.id])
 
     
 
 
     return (
-        <div>
-            
+    <div>
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css"/>
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+            <Link type="button" to="/" class="btn btn-warning" style={{marginTop: 30, marginLeft: 30}}>Back</Link>
+        <div class="container emp-profile">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="profile-img">
+                        <img src={data.Profile.photoProfile} alt=""/>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="profile-head">
+                        <h5>
+                            {data.Profile.fullName}
+                        </h5>
+                        <h6>
+                            {data.role}
+                        </h6>
+                        <ul class="nav nav-tabs" id="myTab" role="tablist" style={{display:"inline"}}>
+                            <li class="nav-item">
+                                <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">About</a>
+                            </li>
+                            <li class="nav-item">
+                            <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <label>Name</label>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <p>{data.Profile.fullName}</p>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <label>Email</label>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <p>{data.email}</p>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <label>Phone</label>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <p>{data.Profile.phoneNumber}</p>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <label>Address</label>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <p>{data.Profile.address}</p>
+                                    </div>
+                                </div>
+                            </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="tab-content profile-tab" id="myTabContent">
+                    
+                        <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <label>Experience</label>
+                                </div>
+                                <div class="col-md-6">
+                                    <p>Expert</p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <label>Hourly Rate</label>
+                                </div>
+                                <div class="col-md-6">
+                                    <p>10$/hr</p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <label>Total Projects</label>
+                                </div>
+                                <div class="col-md-6">
+                                    <p>230</p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <label>English Level</label>
+                                </div>
+                                <div class="col-md-6">
+                                    <p>Expert</p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <label>Availability</label>
+                                </div>
+                                <div class="col-md-6">
+                                    <p>6 months</p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <label>Your Bio</label><br/>
+                                    <p>Your detail description</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+    </div>
     );
 };
 
